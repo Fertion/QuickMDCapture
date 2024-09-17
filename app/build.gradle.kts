4bcfa0d619e245e44f924d8179abbe9846f081b0
DMITRY_FERT <fertion@list.ru>
2024-09-17 23:57:31 +0300

    release 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.quickmdcapture"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
